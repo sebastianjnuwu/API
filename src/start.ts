2b29a65298ec400f911802efeeeb5dc1d7c56a8d
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-08-23 18:41:30 +0000

    up

--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,6 @@
 // Hello World 
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient()
 import express from 'express';
 const app = express();
-
-app.use(express.json());
-app.get('/', (req, res) => res.json({ message: 'Working!' }));
 
 app.listen(8080, () => {
   console.log('[ Info ] Web online!')
