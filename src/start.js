5a3abac9694066e838fd9ee83a39521c226a3d02
sebastianjnuwu <sebastianjnuwu@gmail.com>
2022-08-23 14:04:36 -0300

    start...

--- src/start.js
+++ src/start.js
@@ -3,6 +3,9 @@ import express from 'express';
 import colors from 'colors';
 const app = express();
 
+app.use(express.json());
+app.get('/', (req, res) => res.json({ message: 'Working!' }));
+
 app.listen(8080, () => 
   console.log(colors.brightGreen('[ Info ] ') + 'Web server bound on port: 8080') 
 );
