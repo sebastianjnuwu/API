be4841104227bc749765be744801d420a07d677f
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-08-24 16:56:24 +0000

    adding...

--- src/start.ts
+++ src/start.ts
@@ -1,7 +1,12 @@
 // Hello World 
 import express from 'express';
+import colors from 'colors';
 const app = express();
 
+app.get('/', function(req, res): void {
+        res.send('hello world');
+});
+
 app.listen(8080, () => {
-  console.log('[ Info ] Web online!')
+  console.log(colors.green('[ Info ] ') + 'Web serve: connected to port 8080');
 });
