0c3193794a884b7ce0f4297aeefea51690e3c910
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-08-25 15:54:28 -0300

    Update connection.ts
    Signed-off-by: SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,11 +2,11 @@
 import { Sequelize } from 'sequelize-typescript';
 
 const connection = new Sequelize({
-	host: '190.115.196.20',
-    database: process.env.DATABASE,
-    dialect: 'mysql',
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD
+   dialect: 'mysql',
+   host: '190.115.196.20',
+   database: process.env.DATABASE,
+   username: process.env.USERNAME,
+   password: process.env.PASSWORD
 })
 
 export default connection;
