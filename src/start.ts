b357d95a23163798ec1b2d562abb3b505f7005eb
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-08-27 12:50:48 +0000

    up

--- src/start.ts
+++ src/start.ts
@@ -3,6 +3,11 @@ import colors from 'colors';
 import ip from 'ip';
 const app = express();
 
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({ KettraWorld: ip.address() });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(colors.green('[ Info ] ') + `Web serve connected: ${ip.address()}:${process.env.PORT}`);
 });    
